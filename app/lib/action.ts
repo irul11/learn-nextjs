78be13892bae7ab82c1dad56aa9e795aa74edc81
irul11 <amtaqiy11@gmail.com>
2024-06-10 17:02:31 +0700

    chapter 15 (adding authentication)

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -4,6 +4,28 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError, CredentialsSignin } from "next-auth";
+
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      if (error.cause?.err instanceof CredentialsSignin) {
+        return 'Invalid credentials.';
+      }
+
+      return 'Something went wrong.';
+
+    }
+    throw error;
+  }
+}
 
 const FormSchema = z.object({
   id: z.string(),
